5c4447c4c97bc26a1acbe0a90d30e83ead26dde9
Alexander Klink <git@alech.de>
2009-12-11 17:32:20 +0100

    output formatting

--- check_formhistory_for_cc.rb
+++ check_formhistory_for_cc.rb
@@ -7,8 +7,12 @@ require 'creditcard'
 Dir.glob(File.expand_path("~") + "/.mozilla/firefox/*/formhistory.sqlite") do |d|
     db = SQLite3::Database.new(d)
     rows = db.execute("SELECT fieldname, value, firstUsed from moz_formhistory ORDER by firstUsed DESC")
-    rows.select { |r| r[1].creditcard? && /^[\d\s]+$/.match(r[1]) && r[1].creditcard_type != 'unknown' }.each do |r|
+    rows.select { |r| r[1].creditcard? && r[1].creditcard_type != 'unknown' }.each do |r|
         time = Time.at(r[2].to_f / 10**6).strftime("%Y/%m/%d %H:%M:%S")
-        puts "Possible credit card found: #{r[1].creditcard_type.capitalize} #{r[1]} in form field #{r[0]} entered at #{time}" 
+        puts "Possible credit card found: "
+        puts " #{r[1].creditcard_type.capitalize} #{r[1]}"
+        puts " in form field #{r[0]}"
+        puts " entered at #{time}" 
+        puts
     end
 end
